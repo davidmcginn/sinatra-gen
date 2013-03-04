42641942ff69f8157056824c72929e009df408ef
Dave McGinn <davidmcginn@gmail.com>
2013-03-04 23:15:51 +0000

    Few changes to the executable and update README.md

---
+++ README.md
@@ -0,0 +1,13 @@
+# SinatraGen
+
+## Installation
+
+Use shell script:
+
+	./rebuild.sh
+
+## Usage
+
+Generate skeleton app with:
+
+	sinatra-gen [app-name]
--- lib/generators/structure_generator.rb
+++
@@ -1,5 +0,0 @@
-class StructureGenerator < Rails::Generators::Base
-  def create_all_files
-    create_file "config/test.rb", "# Add initialization content here"
-  end
-end
